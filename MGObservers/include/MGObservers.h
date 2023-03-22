8f8932e444868ac45209550b938a7f1c38986f15
Matt <matt@bigpaua.com>
2023-03-22 14:30:09 +0900

    fix for swiftpm

--- MGObservers/include/MGObservers.h
+++ MGObservers/include/MGObservers.h
@@ -2,6 +2,9 @@
 //  Created by matt on 9/06/14.
 //
 
+#import "MGObserver.h"
+#import "MGBlockWrapper.h"
+#import "MGDeallocAction.h"
+#import "MGWeakHandler.h"
 #import "NSObject+MGObservers.h"
 #import "UIControl+MGObservers.h"
-
